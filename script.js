e75e0612f47f88b1884bd9961d3eca46ac4a96e1
John Muir <johnmuir@Johns-MacBook-Pro.local>
2020-02-05 10:22:42 +0000

    message

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let dice = document.getElementById("dice")
+let dice1 = document.getElementById("dice")
 let score1 = document.getElementById("score")
 let count1 = document.getElementById("count")
 let win1 = document.getElementById("win")
@@ -12,27 +12,27 @@ document.getElementById("button").addEventListener("click", () => {
     count++
     count1.textContent = count
     if(num === 1){
-        dice.src = "https://upload.wikimedia.org/wikipedia/commons/2/2c/Alea_1.png"
+        dice1.style.transform = "translateZ(-100px) rotateY(0deg)"
         score = 0
         score1.textContent = score
     } else if(num === 2){
-        dice.src = "https://upload.wikimedia.org/wikipedia/commons/b/b8/Alea_2.png"
+        dice1.style.transform = "translateZ(-100px) rotateY(90deg)"
         score += 2
         score1.textContent = score
     } else if(num === 3){
-        dice.src = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Alea_3.png"
+        dice1.style.transform = "translateZ(-100px) rotateX(-90deg)"
         score += 3
         score1.textContent = score
     } else if(num === 4){
-        dice.src = "https://upload.wikimedia.org/wikipedia/commons/8/8d/Alea_4.png"
+        dice1.style.transform = "translateZ(-100px) rotateX(90deg)"
         score += 4
         score1.textContent = score
     } else if(num === 5){
-        dice.src = "https://upload.wikimedia.org/wikipedia/commons/5/55/Alea_5.png"
+        dice1.style.transform = "translateZ(-100px) rotateY(-90deg)"
         score += 5
         score1.textContent = score
     } else if(num === 6){
-        dice.src = "https://upload.wikimedia.org/wikipedia/commons/f/f4/Alea_6.png"
+        dice1.style.transform = "translateZ(-100px) rotateY(-180deg)"
         score += 6
         score1.textContent = score
     }
